216c4cb355f041da7493a53e147fc33b0b67b413
pvanborre <paul.vanborre-jamin@polytechnique.edu>
2023-11-30 02:54:06 +0100

    code propre pour les revenue functions

--- codes/utils_paper.py
+++ codes/utils_paper.py
@@ -3,12 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 import click
-from statsmodels.nonparametric.kernel_regression import KernelReg
-from statsmodels.nonparametric.smoothers_lowess import lowess
-import statsmodels.api as sm
 
 
 def mtr(total_earning, taux_marginal, period):
+    """
+    takes as input marginal tax rates and the total earnings of the foyers_fiscal
+    computes mtr for the grid x_values of total_earnings we are considering
+    """
     sorted_indices = np.argsort(total_earning)
     total_earning = total_earning[sorted_indices]
     taux_marginal = taux_marginal[sorted_indices]
@@ -28,6 +29,11 @@ def mtr(total_earning, taux_marginal, period):
     return ipol_MTR
 
 def earning_adapted_mtr(earning, total_earning, ipol_MTR, period):
+    """
+    we need to convert the marginal tax rates that we got in the mtr function from mtr on a grid of total_earnings to a grid of primary/secondary earnings
+    for this we start from the grid of primary earnings, we look for the primary earning value associated to it, therefore we can retreive the total earning value, 
+    and then the total earning sampled value that is the closest and finally the mtr associated
+    """
     earning_sampled = np.linspace(np.percentile(earning, 1), np.percentile(earning, 99.9), 100)
     total_earning_sampled = np.linspace(np.percentile(total_earning, 1), np.percentile(total_earning, 99.9), 100)
 
@@ -57,9 +63,9 @@ def earning_adapted_mtr(earning, total_earning, ipol_MTR, period):
 
 
 
-def primary_cdf_pdf(earning, taux_marginal, weights, elasticity, period):
+def compute_intensive_revenue_function(earning, ipol_MTR_earning, weights, elasticity, period):
     """
-    TODO
+    Computes the pdf and cdf of earnings, and the intensive revenue function
     """
 
     earning.sort()
@@ -70,51 +76,49 @@ def primary_cdf_pdf(earning, taux_marginal, weights, elasticity, period):
     pdf = kde(x_values)
     cdf = np.cumsum(pdf) / np.sum(pdf)
 
-    ipol_MTR_test_case_baseline = np.interp(x_values, earning, taux_marginal)
-    
-
-    behavioral = - x_values * pdf * elasticity * ipol_MTR_test_case_baseline/(1-ipol_MTR_test_case_baseline)  
+    behavioral = - x_values * pdf * elasticity * ipol_MTR_earning/(1-ipol_MTR_earning)  
     mechanical = 1 - cdf
     intensive_revenue_function = behavioral + mechanical
 
-    plt.plot(x_values, cdf, label='CDF')   
+    return x_values, cdf, pdf, intensive_revenue_function
+
+def plot_intensive_revenue_function(primary_grid, cdf_primary, pdf_primary, intensive_primary_revenue_function, secondary_grid, cdf_secondary, pdf_secondary, intensive_secondary_revenue_function, period):
+    
+
+    plt.plot(primary_grid, cdf_primary, label='primary') 
+    plt.plot(secondary_grid, cdf_secondary, label='secondary')   
     plt.xlabel('Gross income')
     plt.ylabel('CDF')
     plt.title("Cumulative distribution function - {annee}".format(annee = period))
     plt.legend()
     plt.show()
-    plt.savefig('../outputs/test_cdf/graphe_B13_{annee}.png'.format(annee = period))
+    plt.savefig('../outputs/test_cdf/cdf_{annee}.png'.format(annee = period))
     plt.close()
-
-    plt.plot(x_values, pdf, label='PDF')   
+    
+    plt.plot(primary_grid, pdf_primary, label='primary') 
+    plt.plot(secondary_grid, pdf_secondary, label='secondary')   
     plt.xlabel('Gross income')
     plt.ylabel('PDF')
     plt.title("Probability distribution function - {annee}".format(annee = period))
     plt.legend()
     plt.show()
-    plt.savefig('../outputs/test_cdf/graphe_B14_{annee}.png'.format(annee = period))
+    plt.savefig('../outputs/test_cdf/pdf_{annee}.png'.format(annee = period))
     plt.close()
 
-    plt.plot(x_values, ipol_MTR_test_case_baseline/(1-ipol_MTR_test_case_baseline), label='MTR ratio')   
+    plt.plot(primary_grid, intensive_primary_revenue_function, label='primary') 
+    plt.plot(secondary_grid, intensive_secondary_revenue_function, label='secondary')    
     plt.xlabel('Gross income')
-    plt.ylabel('MTR ratio')
-    plt.title("MTR ratio - {annee}".format(annee = period))
-    plt.legend()
-    plt.show()
-    plt.savefig('../outputs/test_cdf/mtr_ratio_{annee}.png'.format(annee = period))
-    plt.close()
-
-    plt.plot(x_values, intensive_revenue_function, label='MTR ratio')   
-    plt.xlabel('Gross income')
-    plt.ylabel('MTR ratio')
-    plt.title("MTR ratio - {annee}".format(annee = period))
+    plt.ylabel('Integral revenue function')
+    plt.title("Integral revenue function - {annee}".format(annee = period))
     plt.legend()
     plt.show()
     plt.savefig('../outputs/test_cdf/revenue_function_{annee}.png'.format(annee = period))
     plt.close()
 
-    integral_trap_primary = np.trapz(intensive_revenue_function, x_values)
-    print("Integral of smoothed_y primary", integral_trap_primary)
+    integral_trap_primary = np.trapz(intensive_primary_revenue_function, primary_grid)
+    print("Primary revenue function integral", integral_trap_primary)
+    integral_trap_secondary = np.trapz(intensive_secondary_revenue_function, secondary_grid)
+    print("Secondary revenue function integral", integral_trap_secondary)
 
 
 
@@ -132,15 +136,29 @@ def launch_utils(annee = None, want_to_mute_decote = None):
         taux_marginal = work_df['taux_marginal'].values,
         period = annee)
     
-    earning_adapted_mtr(earning=work_df['primary_earning'].values,
+    ipol_MTR_primary = earning_adapted_mtr(earning=work_df['primary_earning'].values,
+                        total_earning=work_df['total_earning'].values,
+                        ipol_MTR=ipol_MTR,
+                        period=annee)
+    
+    ipol_MTR_secondary = earning_adapted_mtr(earning=work_df['secondary_earning'].values,
                         total_earning=work_df['total_earning'].values,
                         ipol_MTR=ipol_MTR,
                         period=annee)
 
-    # primary_cdf_pdf(earning = work_df['primary_earning'].values, 
-    #                 taux_marginal = work_df['taux_marginal'].values,
-    #                 weights = work_df['wprm'].values, 
-    #                 elasticity = 0.25,
-    #                 period = annee)
+    primary_grid, cdf_primary, pdf_primary, intensive_primary_revenue_function = compute_intensive_revenue_function(earning = work_df['primary_earning'].values, 
+                    ipol_MTR_earning = ipol_MTR_primary,
+                    weights = work_df['wprm'].values, 
+                    elasticity = 0.25,
+                    period = annee)
+    
+    secondary_grid, cdf_secondary, pdf_secondary, intensive_secondary_revenue_function = compute_intensive_revenue_function(earning = work_df['secondary_earning'].values, 
+                    ipol_MTR_earning = ipol_MTR_secondary,
+                    weights = work_df['wprm'].values, 
+                    elasticity = 0.25,
+                    period = annee)
+    
+    plot_intensive_revenue_function(primary_grid, cdf_primary, pdf_primary, intensive_primary_revenue_function, secondary_grid, cdf_secondary, pdf_secondary, intensive_secondary_revenue_function, annee)
+    
 
 launch_utils()
