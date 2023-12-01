711b4300e401ce3a3322b34324b3982c8300e846
pvanborre <paul.vanborre-jamin@polytechnique.edu>
2023-12-01 21:59:37 +0100

    ajout lowess other way to regress

--- codes/utils_paper.py
+++ codes/utils_paper.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 import click
 from statsmodels.nonparametric.kernel_regression import KernelReg
+import statsmodels.api as sm
+from scipy.interpolate import interp1d
 
 
-
-
-
-
-def mtr_v2(earning, taux_marginal, weights, period):
+def computes_mtr_ratios_knowing_earning(earning, taux_marginal, weights, period):
+    """
+    This takes as input numpy arrays of size number foyers_fiscaux, primary or secondary earnings, and then the marginal tax rates and the weights
+    This computes E(T'/(1-T') | yp/s) for all distinct values of primary earings (or secondary earnings)
+    So it returns this numpy array of size unique primary/secondary earnings
+    """
     mtr_ratio = taux_marginal/(1-taux_marginal)
 
     unique_earning = np.unique(earning)
@@ -21,22 +24,23 @@ def mtr_v2(earning, taux_marginal, weights, period):
         mean_tax_rate = np.average(mtr_ratio[indices], weights=weights[indices])
         mean_tax_rates[i] = mean_tax_rate
 
-    # plt.scatter(unique_earning, mean_tax_rates, label='MTR ratio')   
-    # plt.xlabel('Gross income')
-    # plt.ylabel('MTR ratio')
-    # plt.title("MTR ratio - {annee}".format(annee = period))
-    # plt.legend()
-    # plt.show()
-    # plt.savefig('../outputs/test_cdf/mtr_ratio3_{annee}.png'.format(annee = period))
-    # plt.close()
-
     return unique_earning, mean_tax_rates
 
-def find_closest_earning_and_tax_rate(original_earnings, average_marginal_tax_rates, period):
+
+def find_closest_earning_and_tax_rate(original_earnings, average_ratios, period):
+    """
+    This takes as input numpy arrays of size unique primary/secondary earnings computed by the above function, 
+    that is distinct primary/secondary earnings and the ratios E(T'/(1-T'))
+    The function creates a grid of earnings and for each earning of the grid, find the closest "real" earning and the corresponding MTR ratio
+    Then it scatters these ratios.
+    Since it is a stairs function with stairs overlapping, we decided to smooth the function using a kernel, and we plot the results
+    """
+
+
     grid_earnings = np.linspace(np.percentile(original_earnings, 1), np.percentile(original_earnings, 99.9), 1000)
 
     closest_indices = np.argmin(np.abs(original_earnings[:, None] - grid_earnings), axis=0)
-    closest_mtr_ratios = average_marginal_tax_rates[closest_indices]
+    closest_mtr_ratios = average_ratios[closest_indices]
 
     plt.scatter(grid_earnings, closest_mtr_ratios, label='MTR ratio')   
     plt.xlabel('Gross income')
@@ -47,7 +51,7 @@ def find_closest_earning_and_tax_rate(original_earnings, average_marginal_tax_ra
     plt.savefig('../outputs/test_cdf/mtr_ratio2_{annee}.png'.format(annee = period))
     plt.close()
 
-    bandwidth = 6000
+    bandwidth = 5000
     kernel_reg = KernelReg(endog=closest_mtr_ratios, exog=grid_earnings, var_type='c', reg_type='ll', bw=[bandwidth], ckertype='gaussian')
     smoothed_y_primary, _ = kernel_reg.fit()
     plt.plot(grid_earnings, smoothed_y_primary, label='MTR')   
@@ -59,7 +63,21 @@ def find_closest_earning_and_tax_rate(original_earnings, average_marginal_tax_ra
     plt.savefig('../outputs/test_cdf/ratio_exp_{bandwidth}_{annee}.png'.format(bandwidth=bandwidth, annee = period))
     plt.close()
 
-    return closest_mtr_ratios
+    # another approach lowess : gives almost the same results as before 
+    # lowess = sm.nonparametric.lowess(closest_mtr_ratios, grid_earnings, frac = 0.25)
+    # smoothed_y_primary = lowess[:, 1]
+    # plt.plot(grid_earnings, smoothed_y_primary, label='MTR')   
+    # plt.xlabel('Gross income')
+    # plt.ylabel('MTR')
+    # plt.title("MTR - {annee}".format(annee = period))
+    # plt.legend()
+    # plt.show()
+    # plt.savefig('../outputs/test_cdf/ratio_exp_lowess_{annee}.png'.format(annee = period))
+    # plt.close()
+
+
+
+    return smoothed_y_primary
 
 
 
@@ -140,37 +158,38 @@ def plot_intensive_revenue_function(primary_grid, primary_earning, cdf_primary, 
 def launch_utils(annee = None, want_to_mute_decote = None):
     work_df = pd.read_csv(f'./excel/{annee}/married_25_55_{annee}.csv')
     print(work_df)
+    # TODO here get rid of the bottom 5% of the distribution (in terms of total earnings i think)
 
     
-    unique_primary_earning, primary_mean_tax_rates = mtr_v2(earning = work_df['primary_earning'].values, 
+    unique_primary_earning, primary_mean_tax_rates = computes_mtr_ratios_knowing_earning(earning = work_df['primary_earning'].values, 
                                             taux_marginal = work_df['taux_marginal'].values, 
                                             weights = work_df['wprm'].values,
                                             period = annee)
     
     primary_mtr_ratios_grid = find_closest_earning_and_tax_rate(original_earnings = unique_primary_earning, 
-                                                        average_marginal_tax_rates = primary_mean_tax_rates, 
+                                                        average_ratios = primary_mean_tax_rates, 
                                                         period = annee)
     
-    unique_secondary_earning, secondary_mean_tax_rates = mtr_v2(earning = work_df['secondary_earning'].values, 
+    unique_secondary_earning, secondary_mean_tax_rates = computes_mtr_ratios_knowing_earning(earning = work_df['secondary_earning'].values, 
                                             taux_marginal = work_df['taux_marginal'].values, 
                                             weights = work_df['wprm'].values,
                                             period = annee)
     
     secondary_mtr_ratios_grid = find_closest_earning_and_tax_rate(original_earnings = unique_secondary_earning, 
-                                                        average_marginal_tax_rates = secondary_mean_tax_rates, 
+                                                        average_ratios = secondary_mean_tax_rates, 
                                                         period = annee)
     
 
     primary_grid, cdf_primary, pdf_primary, intensive_primary_revenue_function = compute_intensive_revenue_function(earning = work_df['primary_earning'].values, 
                     mtr_ratios_grid = primary_mtr_ratios_grid,
                     weights = work_df['wprm'].values, 
-                    elasticity = 0.25,
+                    elasticity = 0.75,
                     period = annee)
     
     secondary_grid, cdf_secondary, pdf_secondary, intensive_secondary_revenue_function = compute_intensive_revenue_function(earning = work_df['secondary_earning'].values, 
                     mtr_ratios_grid = secondary_mtr_ratios_grid,
                     weights = work_df['wprm'].values, 
-                    elasticity = 0.75,
+                    elasticity = 0.25,
                     period = annee)
     
     plot_intensive_revenue_function(primary_grid, work_df['primary_earning'].values, cdf_primary, pdf_primary, intensive_primary_revenue_function, secondary_grid, work_df['secondary_earning'].values, cdf_secondary, pdf_secondary, intensive_secondary_revenue_function, work_df['wprm'].values, annee)
