fe4ad122e5ff16af8b82d52fe1b4a42a9b2f3643
pvanborre <paul.vanborre-jamin@polytechnique.edu>
2024-01-02 16:29:08 +0100

    on cree les subfolders de output dans le bash script

--- codes/graphe15.sh
+++ codes/graphe15.sh
@@ -27,6 +27,29 @@ echo "output_graphe_15.txt has been created."
 ################################################################################
 
 
+################################################################################
+# For the first use, creates the folder excel and its year subfolders
+excel_folder="./excel"
+
+# Create the excel folder if it does not exist
+if [ ! -d "$excel_folder" ]; then
+    mkdir "$excel_folder"
+    echo "Created 'excel' folder."
+fi
+
+# Create subfolders 2002 to 2019 if they do not exist
+for year_folder in {2002..2019}; do
+    year_folder_path="$excel_folder/$year_folder"
+
+    if [ ! -d "$year_folder_path" ]; then
+        mkdir "$year_folder_path"
+        echo "Created '$year_folder' subfolder."
+    fi
+done
+
+echo "Folder and subfolders creation completed."
+################################################################################
+
 
 ################################################################################
 # Delete all csv files that store data 
@@ -46,6 +69,25 @@ echo "Process completed."
 ################################################################################
 
 
+################################################################################
+# For the first use, creates the subfolders of the outputs folder 
+cd ../outputs
+
+subfolders=("cdf_primary_secondary" "median_share_primary" "revenue_functions"  "winners_political_economy" "extensive_expectation_primary" "mtr_ratio_by_primary" "participation_elasticity" "welfare" "extensive_expectation_secondary"  "mtr_ratio_by_secondary"  "pdf_primary_secondary" "winners_over_time")
+
+for subfolder in "${subfolders[@]}"; do
+    subfolder_path="$subfolder"
+
+    if [ ! -d "$subfolder_path" ]; then
+        mkdir "$subfolder_path"
+        echo "Created '$subfolder' subfolder."
+    fi
+done
+
+echo "Folder and subfolders creation completed."
+
+cd ../codes
+################################################################################
 
 ################################################################################
 # Delete all png files that store outputs 
