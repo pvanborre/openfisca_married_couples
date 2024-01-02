368902929b56b9c044586e3710aa8e25113d38eb
pvanborre <paul.vanborre-jamin@polytechnique.edu>
2024-01-02 15:18:03 +0100

    on ajoute un process de suppression des excel avant de faire tourner a nouveau l'ensemble

--- codes/graphe15.sh
+++ codes/graphe15.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Delete output_graphe_15.txt that will store outputs
 if [ -e 'output_graphe_15.txt' ]; then
     rm 'output_graphe_15.txt'
     echo "output_graphe_15.txt has been removed."
@@ -9,6 +10,25 @@ fi
 
 touch "output_graphe_15.txt"
 echo "output_graphe_15.txt has been created."
+
+# Delete all csv files that store data 
+
+main_folder_path="./excel"
+
+for year_folder in {2002..2019}; do
+    year_folder_path="$main_folder_path/$year_folder"
+
+    if [ -d "$year_folder_path" ]; then
+        find "$year_folder_path" -name "*.csv" -type f -delete
+        echo "All CSV files deleted in $year_folder_path"
+    else
+        echo "$year_folder_path does not exist."
+    fi
+done
+
+echo "Process completed."
+
+
 
 for i in $(seq 2002 2019);
 do
