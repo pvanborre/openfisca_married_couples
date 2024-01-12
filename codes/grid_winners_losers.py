daa4039718472d9b69b568d6b285bed9976711bc
pvanborre <paul.vanborre-jamin@polytechnique.edu>
2024-01-12 20:04:55 +0100

    still words need code want to use dictionary approach but not that easy

--- codes/grid_winners_losers.py
+++ codes/grid_winners_losers.py
@@ -23,3 +23,49 @@ from filing as a married couple to the total income of the couple, i.e. Tm(y1+y2
 # code reform that computes a primary tax burden that would be as if it was a single man (account for a single declaration)
 # but how to allocate dependents ? we allocate them to one spouse only 
 
+# Daniel stata code 6D
+# goal is to store a new column in data_pre['bonus'] = (data_pre['taux_marginal'] - TODO)/(data_pre['primary_earning'] + data_pre['secondary_earning'])
+
+# compute info for these fake individuals : for the microsimulation model only need to change marital status for one, change marital status and set all dependents to 0 for the other 
+# not that easy : better maybe to used dictionary form or at least look at the IRPP formula again on OF-France
+# dico seems the best thing to do to separate individuals without dealing with id_foy etc 
+# yes but as I considered at the beginning of the project it is not that easy though so ....
+
+
+"""
+Indiv
+activite
+age
+categorie_salarie
+categorie_non_salarie
+chomage_brut
+contrat_de_travail
+date_naissance
+effectif_entreprise
+heures_remunerees_volume
+pensions_alimentaires_percues
+pensions_invalidite
+rag
+retraite_brute
+ric
+rnc
+rpns_imposables
+salaire_de_base
+statut_marital
+primes_fonction_publique
+traitement_indiciaire_brut
+
+Household
+loyer
+statut_occupation_logement
+taxe_habitation
+zone_apl
+logement_conventionne
+
+
+
+Computed
+maries_ou_pacses = False
+primary_earning 
+secondary_earning 
+"""
